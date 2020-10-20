f49042af25ef221ae50fee28ea122d4d2da94dba
Felipe <fee_goomes@hotmail.com>
2020-10-20 16:02:35 -0300

    Adicionadas funções para renderizar o board

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Square(props) {
+    return <button className="square">
+        {props.value}
+    </button>
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i}/>
+    }
+    render() {
+        return <div>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+        </div>
+    }
+}
+
+
+class Game extends React.Component {
+    render() {
+        return (
+        <div className="game">
+            <div className="game-board">
+               <Board/>
+            </div>
+
+            <div className="game-info">
+                Informações do Jogo
+            </div>
+        </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    //Componente -> Elemento a ser renderizado
+    <Game/>,
+    // Em um elemento -> Container em que será renderizado
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
